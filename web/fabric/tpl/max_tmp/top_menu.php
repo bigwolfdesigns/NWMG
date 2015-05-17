1ebbcddfd9f4fe2aafb7d75712def4092a4a038a
Billy Stalnaker <billy@bigwolfdesigns.com>
2015-05-17 00:16:32 -0400

    Fixes template issues needing to be corrected for Maxson Doors.

--- web/fabric/tpl/max_tmp/top_menu.php
+++ web/fabric/tpl/max_tmp/top_menu.php
@@ -4,19 +4,7 @@
 			<img class='logo' src='/images/image/3'/>
 		</a>
 	</div>
-	<div class="col-xs-12 col-sm-12 col-md-7 top-navigation">
-		<ul>
-			<?php
-			foreach($top_nav_bars as $top_nav_bar){
-				$page			 = $top_nav_bar['page'];
-				$name			 = $top_nav_bar['name'];
-				$class_key		 = str_replace('-', '_', lc('uri')->get(CLASS_KEY, ''));
-				$t_page			 = str_replace(array('/', '-'), array('', '_'), substr($page, 0, strpos($page, '.')));
-				$active_class	 = ($t_page==$class_key)?'active':'';
-				?><li class="<?php echo $active_class ?>"><a class="nav-link" href="/<?php echo $page ?>"><?php echo $name ?></a></li><?php }
-			?>
-		</ul>
-	</div>
+	<div class="col-xs-12 col-sm-12 col-md-7"></div>
 	<div class='col-md-2'>
 		<a class='btn btn-default pull-right request-quote-btn' href='/request-quote.html'>Request a Quote</a>
 		<form class='search-form pull-right' action="<?php echo $search_url ?>" method="GET">
@@ -30,4 +18,19 @@
 		<span class='pull-right phone-number'>800-532-6099</span>
 	</div>
 	<div class='clear'></div>
+</div>
+<div class="row nav-row">
+	<div class="col-xs-12 col-sm-12 col-md-12 top-navigation">
+		<ul>
+			<?php
+			foreach($top_nav_bars as $top_nav_bar){
+				$page			 = $top_nav_bar['page'];
+				$name			 = $top_nav_bar['name'];
+				$class_key		 = str_replace('-', '_', lc('uri')->get(CLASS_KEY, ''));
+				$t_page			 = str_replace(array('/', '-'), array('', '_'), substr($page, 0, strpos($page, '.')));
+				$active_class	 = ($t_page==$class_key)?'active':'';
+				?><li class="<?php echo $active_class ?>"><a class="nav-link" href="/<?php echo $page ?>"><?php echo $name ?></a></li><?php }
+			?>
+		</ul>
+	</div>
 </div>
